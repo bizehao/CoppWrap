621f01811189231251e14d7a3885a1c4b9ff35f8
bizehao <18635939450@163.com>
2024-11-25 08:18:19 +0800

    修复bug,还有问题

--- CoppWrap/CoppWrap.cpp
+++ CoppWrap/CoppWrap.cpp
@@ -4,191 +4,118 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
-
 #include <coro/manual_executor.hpp>
 #include <coro/thread_pool.hpp>
 #include <coro/coro_wrap.hpp>
 #include <coro/awaitify.hpp>
-
 #include <noboost/context/fiber.hpp>
-//#include <boost/context/continuation.hpp>
-//#include <boost/asio.hpp>
-
 #include <chrono>
 #include <future>
-
-#include <format>
-
-#include <noboost/pool/pool.hpp>
 #include <chrono>
+#include <coro/manual_executor.hpp>
+#include <functional>
+#include <coro/move_wrapper.hpp>
+#include <sstream>
+#include <mutex>
 
 cw::ManualExecutor manualExecutor;
-cw::ThreadPool threadPool;
-cw::ThreadPool threadPoolBB;
+cw::ThreadPool threadPoolA;
+cw::ThreadPool threadPoolB;
+cw::ThreadPool threadPoolC;
 
-//
-template <typename Obj, typename MemFun, typename Fun>
-void connect(Obj* obj, MemFun memFUn, Fun&& fun) {
+struct sync_count : std::stringstream
+{
+    ~sync_count()
+    {
+        static std::mutex _mutex;
+        std::lock_guard guard{ _mutex };
+        std::cout << this->rdbuf();
+    }
+};
+
+template<typename Obj, typename MemFun, typename Fun>
+void connect(Obj* obj, MemFun memFUn, Fun&& fun)
+{
     obj->_fun = fun;
 }
 
-class Button {
+class Button
+{
 public:
-    void click(bool checked) {
+    void click(bool checked)
+    {
         _fun(checked);
     }
 
     std::function<void(bool)> _fun;
 };
 
-void taskFun(int sec) {
-    runOn(threadPool);
-    std::cout << "4 taskFun中执行耗时任务: " << std::this_thread::get_id() << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds{sec});
-}
-
 using namespace std::chrono_literals;
+namespace ctx = boost::context;
 
-//#define My_Test
-
-const int MAXLENGTH = 10000;
-
-int main() {
+int main()
+{
     system("chcp 65001");
 
-    boost::pool<> p(sizeof(int));
-    int* vec1[MAXLENGTH];
-    int* vec2[MAXLENGTH];
-
-    auto clock_begin = std::chrono::steady_clock::now();
-    for (int i = 0; i < MAXLENGTH; ++i) {
-        vec1[i] = static_cast<int*>(p.malloc());
-    }
-    for (int i = 0; i < MAXLENGTH; ++i) {
-        p.free(vec1[i]);
-        vec1[i] = NULL;
-    }
-
-    auto clock_end = std::chrono::steady_clock::now();
-    std::cout << "程序运行了 " << (clock_end - clock_begin).count() << " 个系统时钟" << std::endl;
-
-    clock_begin = std::chrono::steady_clock::now();
-    for (int i = 0; i < MAXLENGTH; ++i) {
-        vec2[i] = new int;
-    }
-    for (int i = 0; i < MAXLENGTH; ++i) {
-        delete vec2[i];
-        vec2[i] = NULL;
-    }
-
-    clock_end = std::chrono::steady_clock::now();
-    std::cout << "程序运行了 " << (clock_end - clock_begin).count() << " 个系统时钟" << std::endl;
-
-    return 0;
-
-    /*namespace ctx = boost::context;
-    int data = 0;
-    ctx::fiber f1{[&data](ctx::fiber&& f2) {
-        std::cout << "f1: entered first time: " << data << std::endl;
-        data += 1;
-        f2 = std::move(f2).resume();
-        std::cout << "f1: entered second time: " << data << std::endl;
-        data += 1;
-        f2 = std::move(f2).resume();
-        std::cout << "f1: entered third time: " << data << std::endl;
-        return std::move(f2);
-    }};
-    f1 = std::move(f1).resume();
-    std::cout << "f1: returned first time: " << data << std::endl;
-    data += 1;
-    f1 = std::move(f1).resume();
-    std::cout << "f1: returned second time: " << data << std::endl;
-    data += 1;
-    f1 = std::move(f1).resume_with([&data](ctx::fiber&& f2) {
-        std::cout << "f2: entered: " << data << std::endl;
-        data = -1;
-        return std::move(f2);
-    });
-    std::cout << "f1: returned third time" << std::endl;*/
-    //========================>>>>>>>>>>>>>>>
-    /*namespace ctx = boost::context;
-    namespace asio = boost::asio;
-    asio::io_context io_context;
-    asio::thread_pool thread_pool;
-
-    auto work = asio::make_work_guard(io_context);
-
     {
-        auto co1 = cw::createCoroutineContext([](int a, double b) {
-            std::cout << std::format("a: {}, b: {}", a, b) << std::endl;
-            cw::runOn(threadPool);
-            std::cout << "start exec task" << std::endl;
-            std::this_thread::sleep_for(3s);
-            std::cout << "task finish" << std::endl;
-            cw::runOn(threadPoolBB);
-            std::cout << "exit" << std::endl;
-        });
-        co1.start(100, 3.14);
+        sync_count{} << "主线程: " << std::this_thread::get_id() << std::endl;
+        for (int i = 0; i < 1; i++)
+        {
+            auto timeOut = cw::createCoroutineContext([](int sec) {
+                std::this_thread::sleep_for(std::chrono::seconds{ sec });
+            });
+
+            auto asyncHandle = cw::createCoroutineContext([](std::string arg) {
+                runOn(threadPoolC);
+                sync_count{} << "开始处理异步任务: " << std::this_thread::get_id() << std::endl;
+                sync_count{} << "参数: " << arg << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds{ 3 });
+                sync_count{} << "结束处理异步任务: " << std::this_thread::get_id() << std::endl;
+            });
+
+            Button bitton;
+            connect(
+                &bitton,
+                &Button::click,
+                Coro[timeOut, asyncHandle](bool checked) {
+                    sync_count{} << "当前在主线程: " << std::this_thread::get_id() << std::endl;
+                    runOn(threadPoolA);
+                    sync_count{} << "切换到子线程A: " << std::this_thread::get_id() << std::endl;
+                    runOn(threadPoolB);
+                    sync_count{} << "切换到子线程B: " << std::this_thread::get_id() << std::endl;
+                    await(timeOut, 3);
+                    sync_count{} << "等待了3s, 继续在线程B: " << std::this_thread::get_id() << std::endl;
+                    runOn(manualExecutor);
+                    sync_count{} << "当前在主线程：" << std::this_thread::get_id() << std::endl;
+                    await(asyncHandle, std::string{ "一些参数" });
+                    sync_count{} << "返回到主线程：" << std::this_thread::get_id() << std::endl;
+                });
+
+            bitton.click(true);
+        }
+        
     }
-    
 
-    io_context.run();*/
+    //验证主线程不阻塞
+    std::thread main_out{
+        []() {
+            while (true)
+            {
+                manualExecutor.post([]() {
+                    //sync_count{} << "--" << std::endl;
+                });
+                std::this_thread::sleep_for(100ms);
+            }
+        }
+    };
 
-#ifdef My_Test
-
-    auto timeOut = cw::createCoroutineContext([](int sec) {
-        await(cw::createCoroutineContext(taskFun), 3);
-        std::cout << "5 taskFun执行完成: " << std::this_thread::get_id() << std::endl;
-        runOn(threadPool);
-        std::cout << "6 timeOut中执行耗时任务: " << std::this_thread::get_id() << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds{sec});
-    });
-
-    std::cout << "0 主线程: " << std::this_thread::get_id() << std::endl;
-    Button bitton;
-    connect(
-        &bitton,
-        &Button::click,
-        Coro[timeOut](bool checked) {
-            std::cout << std::boolalpha;
-            std::cout << "1 在主线程操作: " << std::this_thread::get_id() << " checked: " << checked << std::endl;
-            checked = false;
-            runOn(threadPoolBB);
-            std::cout << "2 切换到子线程操作: " << std::this_thread::get_id() << " checked: " << checked << std::endl;
-            checked = true;
-            std::cout << "3 等待timeOut结束: " << std::this_thread::get_id() << " checked: " << checked << std::endl;
-            await(timeOut, 3);
-            std::cout << "7 timeOut结束了: " << std::this_thread::get_id() << " checked: " << checked << std::endl;
-            runOn(manualExecutor);
-            std::cout << "8 切换到主线程操作: " << std::this_thread::get_id() << " checked: " << checked << std::endl;
-        });
-
-    bitton.click(true);
-#endif // My_Test
-
-    //auto task1 = async::spawn([] {
-    //    std::cout << "exec task1" << std::endl;
-    //    std::this_thread::sleep_for(std::chrono::seconds{3});
-    //    std::cout << "Task 1 executes asynchronously" << std::endl;
-    //    return 100;
-    //});
-
-    //auto timeOut = cw::createCoroutineContext([&task1](int sec) {
-    //    auto rst = cw::await(task1);
-    //    std::cout << "5 taskFun执行完成: " << std::this_thread::get_id() << " rst: " << rst << std::endl;
-    //    runOn(threadPool);
-    //    std::cout << "6 timeOut中执行耗时任务: " << std::this_thread::get_id() << std::endl;
-    //});
-
-    //cw::sync_wait(timeOut, 2);
-    //std::cout << "等待结束" << std::endl;
-
-#ifdef My_Test
-    while (true) {
+    while (true)
+    {
         manualExecutor.wait_for_task();
         manualExecutor.loop_once();
     }
-#endif
+
+    main_out.join();
 
     return 0;
 }
--- include/coro/coro_wrap.hpp
+++ include/coro/coro_wrap.hpp
@@ -3,270 +3,353 @@
 #include <memory>
 #include "function_traits.hpp"
 #include <noboost/context/fiber.hpp>
+#include "move_wrapper.hpp"
 
-namespace cw {
+namespace cw
+{
 
-namespace ctx = boost::context;
+    namespace ctx = boost::context;
 
-class AbstractExecutor;
-class CoroutineContextBase;
+    class AbstractExecutor;
+    class CoroutineContextBase;
 
-class BaseAbstractExecutor {
-public:
-    using task = std::function<void()>;
-    virtual ~BaseAbstractExecutor() = default;
-    virtual void post(task fun) = 0;
-};
+    class BaseAbstractExecutor
+    {
+    public:
+        using task = std::function<void()>;
+        virtual ~BaseAbstractExecutor() = default;
+        virtual void post(task fun) = 0;
+    };
 
-static CoroutineContextBase*& thisCoro() {
-    thread_local static CoroutineContextBase* sCurCoroutine = nullptr;
-    return sCurCoroutine;
-};
+    static CoroutineContextBase*& thisCoro()
+    {
+        thread_local CoroutineContextBase* sCurCoroutine = nullptr;
+        return sCurCoroutine;
+    };
 
-class CoroutineContextBase {
-    friend class AbstractExecutor;
+    class CoroutineContextBase
+    {
+        friend class AbstractExecutor;
 
-public:
-    CoroutineContextBase() {
-    }
-    ~CoroutineContextBase() {
-    }
-    void resume() {
-
-        thisCoro() = this;
-        _source = std::move(_source).resume();
-
-        if (_yield_after_call) {
-            _yield_after_call();
-            _yield_after_call = nullptr;
+    public:
+        CoroutineContextBase()
+        {
+        }
+        ~CoroutineContextBase()
+        {
+        }
+        void resume()
+        {
+            thisCoro() = this;
+            _enter_source = std::move(_output_source).resume();
+            finish_callback();
         }
 
-        finish_callback();
-    }
+        // 恢复到切入点的时侯 call fun
+        /*void yield(std::function<void()> fun = nullptr)
+        {
+            thisCoro() = nullptr;
+            _source = std::move(_source).resume_with([fun = std::move(fun)](ctx::fiber&& fc) {
+                fun();
+                return std::move(fc);
+            });
+            finish_callback();
+        }*/
 
-    //恢复到切入点的时侯 call fun
-    void yield(std::function<void()> fun = nullptr) {
-        thisCoro() = nullptr;
-        _yield_after_call = std::move(fun);
-        _source = std::move(_source).resume();
+        void yield()
+        {
+            thisCoro() = nullptr;
+            _output_source = std::move(_enter_source).resume();
+            finish_callback();
+        }
 
-        finish_callback();
-    }
+        void yield(std::function<ctx::fiber(ctx::fiber&&)> fun)
+        {
+            thisCoro() = nullptr;
+            _output_source = std::move(_enter_source).resume_with(std::move(fun));
+            finish_callback();
+        }
 
-    bool isFinished() {
-        return _is_run_finish;
-    }
+        bool isFinished()
+        {
+            return _is_finished;
+        }
 
-    void setLastCallback(std::function<void()> callback) {
-        _last_call = callback;
-    }
+        void setLastCallback(std::function<void()> callback)
+        {
+            _last_call = callback;
+        }
 
-    BaseAbstractExecutor* getExecutor() {
-        return _executor;
-    }
+        BaseAbstractExecutor* getExecutor()
+        {
+            return _executor;
+        }
 
-protected:
+        void destory()
+        {
+            _output_source = {};
+            finish_callback();
+        }
 
-    void finish_callback() {
-        if (_is_run_finish){
-            if (_last_call) {
-                _last_call();
+        void setEnterFiber(ctx::fiber&& f)
+        {
+            _enter_source = std::move(f);
+        }
+
+        void setOutputFiber(ctx::fiber&& f)
+        {
+            _output_source = std::move(f);
+        }
+
+        void setLastFiber(ctx::fiber&& f)
+        {
+            _last_resume = std::move(f);
+        }
+
+        ctx::fiber getLastFiber()
+        {
+            return std::move(_last_resume);
+        }
+
+    protected:
+        void finish_callback()
+        {
+            if (isFinished())
+            {
+                if (_last_call)
+                {
+                    _last_call();
+                }
+                _prolong_life.reset();
             }
-            _prolong_life.reset();
         }
-    }
 
-    ctx::fiber _source;
-    BaseAbstractExecutor* _executor{nullptr};
+        ctx::fiber _enter_source; //进入
+        ctx::fiber _output_source; //出去
+        ctx::fiber _last_resume;
+        BaseAbstractExecutor* _executor{ nullptr };
+        bool _is_finished{ false };
+        std::function<void()> _last_call;
+        std::shared_ptr<CoroutineContextBase> _prolong_life;
+        friend void runOn(BaseAbstractExecutor& executor);
+    };
 
-    std::function<void()> _yield_after_call;
-    std::function<void()> _last_call;
+    template<typename T>
+    class CoroutineContext;
 
-    bool _is_run_finish{false};
+    template<typename... Args>
+    class CoroutineContext<std::tuple<Args...>> : public CoroutineContextBase, public std::enable_shared_from_this<CoroutineContext<std::tuple<Args...>>>
+    {
+        using ArgsTup = std::tuple<Args...>;
 
-    std::shared_ptr<CoroutineContextBase> _prolong_life;
+        friend class AbstractExecutor;
 
-    friend void runOn(BaseAbstractExecutor& executor);
-};
-
-template <typename T>
-class CoroutineContext;
-
-template <typename... Args>
-class CoroutineContext<std::tuple<Args...>> : public CoroutineContextBase,
-                                              public std::enable_shared_from_this<CoroutineContext<std::tuple<Args...>>> {
-    using ArgsTup = std::tuple<Args...>;
-
-    friend class AbstractExecutor;
-
-public:
-    CoroutineContext(std::function<void(Args...)> fun) :
-        _fun{fun} {
-    }
-
-    ~CoroutineContext() {
-
-    }
-
-    void start(Args... args) {
-        ArgsTup tup = std::forward_as_tuple(args...);
-        startAndOp(tup);
-    }
-
-    void start(ArgsTup tup) {
-        startAndOp(tup);
-    }
-
-private:
-    void startAndOp(ArgsTup tup) {
-        if (_source) {
-            throw std::runtime_error{"Started multiple times"};
+    public:
+        CoroutineContext(std::function<void(Args...)> fun) :
+            _fun{ fun }
+        {
         }
-        _source = ctx::fiber{[self = getSelf(), &tup](ctx::fiber&& fiber) {
-            self->_source = std::move(fiber);
 
-            self->invoke(self->_fun, tup, std::make_index_sequence<std::tuple_size_v<ArgsTup>>{});
-            
-            self->_is_run_finish = true;
-            self->_prolong_life = self;
-            return std::move(self->_source);
-        }};
-        resume();
+        ~CoroutineContext()
+        {
+        }
+
+        void start(Args... args)
+        {
+            ArgsTup tup = std::forward_as_tuple(args...);
+            startAndOp(tup);
+        }
+
+        void start(ArgsTup tup)
+        {
+            startAndOp(tup);
+        }
+
+    private:
+        void startAndOp(ArgsTup tup)
+        {
+            if (_output_source)
+            {
+                throw std::runtime_error{ "Started multiple times" };
+            }
+            _output_source = ctx::fiber{ [self = getSelf(), &tup](ctx::fiber&& fiber) {
+                self->_enter_source = std::move(fiber);
+                self->_prolong_life = self;
+                self->invoke(self->_fun, tup, std::make_index_sequence<std::tuple_size_v<ArgsTup>>{});
+                self->_is_finished = true;
+                return std::move(self->_enter_source);
+            } };
+            resume();
+        }
+
+        template<typename Fun, typename Tup, size_t... I>
+        void invoke(Fun&& fun, Tup&& tup, std::index_sequence<I...>)
+        {
+            fun(std::get<I>(std::forward<Tup>(tup))...);
+        }
+
+        std::shared_ptr<CoroutineContext<std::tuple<Args...>>> getSelf()
+        {
+            using supper_this = std::enable_shared_from_this<CoroutineContext<std::tuple<Args...>>>;
+            return supper_this::shared_from_this();
+        }
+
+    private:
+        std::function<void(Args...)> _fun;
+    };
+
+    using CoroutineContextBasePtr = std::shared_ptr<CoroutineContextBase>;
+
+    template<typename T>
+    using CoroutineContextPtr = std::shared_ptr<CoroutineContext<T>>;
+
+    template<typename T>
+    class CoroutineContextWrap;
+
+    template<typename... Args>
+    class CoroutineContextWrap<std::tuple<Args...>>
+    {
+    public:
+        CoroutineContextWrap(CoroutineContextPtr<std::tuple<Args...>> ptr) :
+            coroPtr{ ptr }
+        {
+        }
+
+        void start(Args... args)
+        {
+            coroPtr->start(args...);
+        }
+
+        void start(std::tuple<Args...> tup)
+        {
+            coroPtr->start(tup);
+        }
+
+        void resume()
+        {
+            coroPtr->resume();
+        }
+
+        void operator()(Args... args)
+        {
+            coroPtr->start(args...);
+        }
+
+        operator CoroutineContextPtr<std::tuple<Args...>>()
+        {
+            return coroPtr;
+        }
+
+        operator std::function<void(Args...)>()
+        {
+            auto lambda = [tmpCoroPtr = coroPtr](Args... args) {
+                tmpCoroPtr->start(args...);
+            };
+            return lambda;
+        }
+
+        CoroutineContextPtr<std::tuple<Args...>> getCoroPtr()
+        {
+            return coroPtr;
+        }
+
+    private:
+        CoroutineContextPtr<std::tuple<Args...>> coroPtr;
+    };
+
+    template<typename Fun, typename ArgsTup = typename detail::function_traits<Fun>::arg_types>
+    CoroutineContextWrap<ArgsTup> createCoroutineContext(Fun&& fun)
+    {
+        static_assert(detail::is_function<Fun>::value);
+        static_assert(std::is_same<typename detail::function_traits<Fun>::return_type, void>::value);
+        return std::make_shared<CoroutineContext<ArgsTup>>(std::forward<Fun>(fun));
     }
 
-    template <typename Fun, typename Tup, size_t... I>
-    void invoke(Fun&& fun, Tup&& tup, std::index_sequence<I...>) {
-        fun(std::get<I>(std::forward<Tup>(tup))...);
-    }
-
-    std::shared_ptr<CoroutineContext<std::tuple<Args...>>> getSelf() {
-        using supper_this = std::enable_shared_from_this<CoroutineContext<std::tuple<Args...>>>;
-        return supper_this::shared_from_this();
-    }
-
-private:
-    std::function<void(Args...)> _fun;
-};
-
-using CoroutineContextBasePtr = std::shared_ptr<CoroutineContextBase>;
-
-template <typename T>
-using CoroutineContextPtr = std::shared_ptr<CoroutineContext<T>>;
-
-template <typename T>
-class CoroutineContextWrap;
-
-template <typename... Args>
-class CoroutineContextWrap<std::tuple<Args...>> {
-public:
-    CoroutineContextWrap(CoroutineContextPtr<std::tuple<Args...>> ptr) :
-        coroPtr{ptr} {
-    }
-
-    void start(Args... args) {
-        coroPtr->start(args...);
-    }
-
-    void start(std::tuple<Args...> tup) {
-        coroPtr->start(tup);
-    }
-
-    void resume() {
-        coroPtr->resume();
-    }
-
-    void operator()(Args... args) {
-        coroPtr->start(args...);
-    }
-
-    operator CoroutineContextPtr<std::tuple<Args...>>() {
-        return coroPtr;
-    }
-
-    operator std::function<void(Args...)>() {
-        auto lambda = [tmpCoroPtr = coroPtr](Args... args) {
-            tmpCoroPtr->start(args...);
-        };
-        return lambda;
-    }
-
-    CoroutineContextPtr<std::tuple<Args...>> getCoroPtr() {
-        return coroPtr;
-    }
-
-private:
-    CoroutineContextPtr<std::tuple<Args...>> coroPtr;
-};
-
-template <typename Fun, typename ArgsTup = typename detail::function_traits<Fun>::arg_types>
-CoroutineContextWrap<ArgsTup> createCoroutineContext(Fun&& fun) {
-    static_assert(detail::is_function<Fun>::value);
-    static_assert(std::is_same<typename detail::function_traits<Fun>::return_type, void>::value);
-    return std::make_shared<CoroutineContext<ArgsTup>>(std::forward<Fun>(fun));
-}
-
-namespace detail {
-template <typename ArgsTup, size_t... I>
-void awaitImpl(std::shared_ptr<CoroutineContext<ArgsTup>> coroPtr,
-               std::index_sequence<I...>,
-               std::tuple_element_t<I, ArgsTup>... args) {
-    CoroutineContextBase* currentCoro = thisCoro();
-    if (currentCoro == nullptr) {
-        throw std::runtime_error{"currentCoro is null"};
-    } else {
-        coroPtr->setLastCallback([currentCoro]() {
-            if (currentCoro->getExecutor() != nullptr) {
-                currentCoro->getExecutor()->post([currentCoro]() {
-                    currentCoro->resume();
+    namespace detail
+    {
+        template<typename ArgsTup, size_t... I>
+        void awaitImpl(std::shared_ptr<CoroutineContext<ArgsTup>> coroPtr,
+                       std::index_sequence<I...>,
+                       std::tuple_element_t<I, ArgsTup>... args)
+        {
+            CoroutineContextBase* currentCoro = thisCoro();
+            if (currentCoro == nullptr)
+            {
+                throw std::runtime_error{ "currentCoro is null" };
+            }
+            else
+            {
+                coroPtr->setLastCallback([coroPtr, currentCoro]() {
+                    if (currentCoro->getExecutor() != nullptr)
+                    {
+                        currentCoro->getExecutor()->post([currentCoro, coroPtr]() {
+                            currentCoro->setOutputFiber(coroPtr->getLastFiber());
+                            currentCoro->resume();
+                        });
+                    }
+                    else
+                    {
+                        currentCoro->setOutputFiber(coroPtr->getLastFiber());
+                        currentCoro->resume();
+                    }
                 });
-            } else {
-                currentCoro->resume();
+                ArgsTup data = std::forward_as_tuple(args...);
+
+                currentCoro->yield([=](ctx::fiber&& f) {
+                    currentCoro->setLastFiber(std::move(f));
+                    coroPtr->start(data);
+                    return ctx::fiber{};
+                });
             }
-        });
-        ArgsTup data = std::forward_as_tuple(args...);
-        currentCoro->yield([=]() {
-            coroPtr->start(data);
+        }
+    } // namespace detail
+
+    template<typename ArgsTup, typename... Args>
+    void await(CoroutineContextPtr<ArgsTup> coroPtr, Args&&... args)
+    {
+        detail::awaitImpl(coroPtr, std::make_index_sequence<std::tuple_size_v<ArgsTup>>{}, std::forward<Args>(args)...);
+    }
+
+    template<typename ArgsTup, typename... Args>
+    void await(CoroutineContextWrap<ArgsTup> coroWrap, Args&&... args)
+    {
+        detail::awaitImpl(coroWrap.getCoroPtr(), std::make_index_sequence<std::tuple_size_v<ArgsTup>>{}, std::forward<Args>(args)...);
+    }
+
+    void runOn(BaseAbstractExecutor& executor)
+    {
+        CoroutineContextBase* coro = thisCoro();
+        coro->_executor = &executor;
+
+        coro->yield([coro, &executor](ctx::fiber&& f) {
+            auto mw_f = folly::makeMoveWrapper(std::move(f));
+            executor.post([mw_f, coro]() mutable {
+                thisCoro() = coro;
+                coro->setEnterFiber(mw_f.move().resume());
+            });
+            return ctx::fiber{};
         });
     }
-}
-} // namespace detail
-
-template <typename ArgsTup, typename... Args>
-void await(CoroutineContextPtr<ArgsTup> coroPtr, Args&&... args) {
-    detail::awaitImpl(coroPtr, std::make_index_sequence<std::tuple_size_v<ArgsTup>>{}, std::forward<Args>(args)...);
-}
-
-template <typename ArgsTup, typename... Args>
-void await(CoroutineContextWrap<ArgsTup> coroWrap, Args&&... args) {
-    detail::awaitImpl(coroWrap.getCoroPtr(), std::make_index_sequence<std::tuple_size_v<ArgsTup>>{}, std::forward<Args>(args)...);
-}
-
-void runOn(BaseAbstractExecutor& executor) {
-    CoroutineContextBase* coro = thisCoro();
-    coro->_executor = &executor;
-    coro->yield([coro, &executor]() {
-        executor.post([coro]() {
-            coro->resume();
-        });
-    });
-}
 
 #define Coro cw::CoroMacroImpl{} +
 
-struct CoroMacroImpl {
-private:
-    template <typename ArgsTup, typename Fun, size_t... I>
-    std::function<void(std::tuple_element_t<I, ArgsTup>...)> makeFun(Fun&& fun, std::index_sequence<I...>) {
-        auto coroFun = createCoroutineContext(std::forward<Fun>(fun));
-        return coroFun;
-    }
+    struct CoroMacroImpl
+    {
+    private:
+        template<typename ArgsTup, typename Fun, size_t... I>
+        std::function<void(std::tuple_element_t<I, ArgsTup>...)> makeFun(Fun&& fun, std::index_sequence<I...>)
+        {
+            auto coroFun = createCoroutineContext(std::forward<Fun>(fun));
+            return coroFun;
+        }
 
-public:
-    template <typename Fun>
-    auto operator+(Fun&& fun) {
-        using arg_types = typename detail::function_traits<Fun>::arg_types;
-        return makeFun<arg_types>(std::forward<Fun>(fun), std::make_index_sequence<std::tuple_size_v<arg_types>>{});
-    }
-};
+    public:
+        template<typename Fun>
+        auto operator+(Fun&& fun)
+        {
+            using arg_types = typename detail::function_traits<Fun>::arg_types;
+            return makeFun<arg_types>(std::forward<Fun>(fun), std::make_index_sequence<std::tuple_size_v<arg_types>>{});
+        }
+    };
 
 } // namespace cw
