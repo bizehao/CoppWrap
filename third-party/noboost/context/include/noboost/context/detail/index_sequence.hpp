9fe1859d9658cb9d5b842f2e71aac8d4a56a21fb
bizehao <18635939450@163.com>
2025-01-22 08:30:42 +0800

    修复一些bug

--- third-party/noboost/context/include/noboost/context/detail/index_sequence.hpp
+++ third-party/noboost/context/include/noboost/context/detail/index_sequence.hpp
@@ -13,42 +13,59 @@
 
 // TODO:
 
-namespace boost {
-namespace context {
-namespace detail {
+namespace boost
+{
+    namespace context
+    {
+        namespace detail
+        {
 
-namespace std_impl {
-template <class _Ty, _Ty... _Vals>
-struct integer_sequence { // sequence of integer parameters
-    static_assert(is_integral_v<_Ty>, "integer_sequence<T, I...> requires T to be an integral type.");
+            namespace std_impl
+            {
+                template<class T, T... I>
+                struct integer_sequence
+                {
+                    template<T N>
+                    using append = integer_sequence<T, I..., N>;
+                    static std::size_t size() { return sizeof...(I); }
+                    using next = append<sizeof...(I)>;
+                    using type = T;
+                };
 
-    using value_type = _Ty;
+                template<class T, T Index, std::size_t N>
+                struct sequence_generator
+                {
+                    using type = typename sequence_generator<T, Index - 1, N - 1>::type::next;
+                };
 
-    static constexpr size_t size() noexcept {
-        return sizeof...(_Vals);
+                template<class T, T Index>
+                struct sequence_generator<T, Index, 0ul>
+                {
+                    using type = integer_sequence<T>;
+                };
+
+                template<class _Ty, _Ty _Size>
+                using make_integer_sequence = typename sequence_generator<_Ty, _Size, _Size>::type;
+
+                template<size_t... _Vals>
+                using index_sequence = integer_sequence<size_t, _Vals...>;
+
+                template<size_t _Size>
+                using make_index_sequence = make_integer_sequence<size_t, _Size>;
+
+                template<class... _Types>
+                using index_sequence_for = make_index_sequence<sizeof...(_Types)>;
+            } // namespace std_impl
+
+            template<std::size_t... I>
+            using index_sequence = std_impl::index_sequence<I...>;
+            template<std::size_t I>
+            using make_index_sequence = std_impl::make_index_sequence<I>;
+            template<typename... T>
+            using index_sequence_for = std_impl::index_sequence_for<T...>;
+
+        }
     }
-};
-
-template <class _Ty, _Ty _Size>
-using make_integer_sequence = __make_integer_seq<integer_sequence, _Ty, _Size>;
-
-template <size_t... _Vals>
-using index_sequence = integer_sequence<size_t, _Vals...>;
-
-template <size_t _Size>
-using make_index_sequence = make_integer_sequence<size_t, _Size>;
-
-template <class... _Types>
-using index_sequence_for = make_index_sequence<sizeof...(_Types)>;
-} // namespace std_impl
-
-template <std::size_t... I>
-using index_sequence = std_impl::index_sequence<I...>;
-template <std::size_t I>
-using make_index_sequence = std_impl::make_index_sequence<I>;
-template <typename... T>
-using index_sequence_for = std_impl::index_sequence_for<T...>;
-
-}}} // namespace boost::context::detail
+} // namespace boost::context::detail
 
 #endif // BOOST_CONTEXT_DETAIL_INDEX_SEQUENCE_H
